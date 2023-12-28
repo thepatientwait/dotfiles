2ad91e2cc15f6adcc7c22e177259cc4bbf678fb3
herb <thepatientwait@gmail.com>
2023-12-28 20:33:37 +1000

    update for void + setup script

---
+++ .setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# install packages
+sudo xbps-install -Su
+sudo xbps-install -y $(cat .package_list)
+
+# custom plugins for i3blocks and nnn
+git clone https://github.com/vivien/i3blocks-contrib ~/.config/i3blocks
+sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
